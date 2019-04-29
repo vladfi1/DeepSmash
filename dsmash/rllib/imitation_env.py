8225b88b65ca82e30ea20e3120a7dea37746a037
Vlad Firoiu <vladfi2@gmail.com>
2019-04-29 09:40:55 +0100

    Cache multiple paths at once.

--- dsmash/rllib/imitation_env.py
+++ dsmash/rllib/imitation_env.py
@@ -30,15 +30,22 @@ def get_data(data_path):
   with open(data_path, 'rb') as f:
     return pickle.load(f)
 
+@functools.lru_cache()
+def get_data_multi(data_paths):
+  data = []
+  for path in data_paths:
+    data.extend(get_data(path))
+
+  total_actions = sum(len(game.state.stage) for game in data)
+  total_frames = sum((game.action.repeat + 1).sum() for game in data)
+  print("Loaded %d games, %d actions, %d frames" %
+      (len(data), total_actions, total_frames))
+
+  return data
+
 class GameReader:
   def __init__(self, data_paths):
-    data = []
-    for path in data_paths:
-      data.extend(get_data(path))
-    total_actions = sum(len(game.state.stage) for game in data)
-    total_frames = sum((game.action.repeat + 1).sum() for game in data)
-    print("Loaded %d games, %d actions, %d frames" %
-        (len(data), total_actions, total_frames))
+    data = get_data_multi(tuple(data_paths))
     self._games = data
     self._game = None
 
--- dsmash/rllib/imitation_trainer.py
+++ dsmash/rllib/imitation_trainer.py
@@ -28,8 +28,8 @@ from dsmash.rllib.model import HumanActionModel
 from tensorflow.python.client import device_lib
 
 def get_available_gpus():
-    local_device_protos = device_lib.list_local_devices()
-    return [x.name for x in local_device_protos if x.device_type == 'GPU']
+  local_device_protos = device_lib.list_local_devices()
+  return [x.name for x in local_device_protos if x.device_type == 'GPU']
 
 print(get_available_gpus())
 import os
